615556d89e26a7e7cb2cbf2f8f3f1e4dbef92b7c
suraj rawat <surajrwat001@gmail.com>
2026-04-30 10:52:17 +0530

    two sum optimal solution  uploaded

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -1,30 +1,65 @@
 // Two Sum
 
+// Brute Force Approach - 
+
 public class TwoSum {
     public static void main(String[] args) {
-        int[] nums = { 2, 7, 11, 15 };
-        int target = 9;
+        int[] nums = { 1, 6, 2, 10, 3 };
+        int target = 7;
 
-        int[] resultIndices = new int[2];
-        boolean found = false;
+        int[] result = twoSum(nums, target);
+        System.err.println(result[0] + ", " + result[1]);
 
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    resultIndices[0] = i;
-                    resultIndices[1] = j;
-                    found = true;
-                    break;
+                    return new int[] { i, j };
                 }
             }
-            if (found)
-                break;
         }
 
-        if (found) {
-            System.out.println("Result indices: [" + resultIndices[0] + ", " + resultIndices[1] + "]");
-        } else {
-            System.out.println("No two numbers add up to target");
-        }
+        return new int[] { -1, -1 };
     }
 }
+
+// Time complexity - O(n²)
+// space complexity - O(1)
+
+/*---------------------------------------------------------------------------------------------------------------------------------------- */
+
+// Optimal Approach (HashMap) -
+
+import java.util.*;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = { 1, 6, 2, 10, 3 };
+        int target = 7;
+
+        int[] result = twoSum(nums, target);
+        System.err.println(result[0] + ", " + result[1]);
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[] { -1, -1 };
+    }
+}
+
+// Time complexity - O(n)
+// space complexity - O(n)

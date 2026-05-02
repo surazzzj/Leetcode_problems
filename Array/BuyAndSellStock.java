05f420c0130b619f02a3dd92b2837ff164af3c98
suraj rawat <surajrwat001@gmail.com>
2026-05-02 10:22:15 +0530

    Added Solution of Buy And sell Stock problem

--- Array/BuyAndSellStock.java
+++ Array/BuyAndSellStock.java
@@ -2,23 +2,59 @@
 
 public class BuyAndSellStock {
     public static void main(String[] args) {
+        int[] arr = { 10, 7, 5, 8, 11, 9 };
+        System.out.println(maxProfit(arr));
+    }
 
-        int[] prices = { 7, 1, 5, 3, 6, 4 };
-
-        int minPrice = Integer.MAX_VALUE;
+    public static int maxProfit(int[] nums) {
         int maxProfit = 0;
 
-        for (int price : prices) {
-            if (price < minPrice) {
-                minPrice = price;
-            }
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int profit = nums[j] - nums[i];
 
-            if (price - minPrice > maxProfit) {
-                maxProfit = price - minPrice;
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
             }
         }
 
-        System.out.println("Maximum profit: " + maxProfit);
-
+        return maxProfit;
     }
 }
+
+// Time complexity - O(n²)
+// Space complexity - O(1)
+
+/*---------------------------------------------------------------------------------------------------------------------------------------- */
+
+// Optimal Approach -
+
+public class BuyAndSellStock {
+    public static void main(String[] args) {
+        int[] nums = { 10, 7, 5, 8, 11, 9 };
+        System.out.println(maxProfit(nums));
+    }
+
+    public static int maxProfit(int[] nums) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < minPrice) {
+                minPrice = nums[i];
+            }
+
+            int profit = nums[i] - minPrice; // profit = selling price - buy price
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+}
+
+// Time complexity - O(n) (best)
+// Space complexity - O(1)

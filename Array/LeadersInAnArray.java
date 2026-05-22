4402a83941c6a128381ea9705732d0c70b3aa078
suraj rawat <surajrwat001@gmail.com>
2026-05-22 09:33:30 +0530

    problem uploaded

--- Array/LeadersInAnArray.java
+++ Array/LeadersInAnArray.java
@@ -2,37 +2,37 @@
 
 // Brute Force Approach - 
 
-// import java.util.*;;
+import java.util.*;
 
-// public class LeadersInAnArray {
-//     public static void main(String[] args) {
-//         int[] nums = { 1, 2, 5, 3, 1, 2 };
-//         System.out.println(findLeaders(nums));
-//     }
+public class LeadersInAnArray {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 5, 3, 1, 2 };
+        System.out.println(findLeaders(nums));
+    }
 
-//     public static List<Integer> findLeaders(int[] nums) {
-//         List<Integer> leaders = new ArrayList<>();
+    public static List<Integer> findLeaders(int[] nums) {
+        List<Integer> leaders = new ArrayList<>();
 
-//         int n = nums.length;
+        int n = nums.length;
 
-//         for (int i = 0; i < n; i++) {
-//             boolean isLeader = true;
+        for (int i = 0; i < n; i++) {
+            boolean isLeader = true;
 
-//             for (int j = i + 1; j < n; j++) {
-//                 if (nums[j] >= nums[i]) {
-//                     isLeader = false;
-//                     break;
-//                 }
-//             }
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] >= nums[i]) {
+                    isLeader = false;
+                    break;
+                }
+            }
 
-//             if (isLeader) {
-//                 leaders.add(nums[i]);
-//             }
-//         }
+            if (isLeader) {
+                leaders.add(nums[i]);
+            }
+        }
 
-//         return leaders;
-//     }
-// }
+        return leaders;
+    }
+}
 
 // Time complexity - O(n²) 
 // Space complexity - O(n)
@@ -41,7 +41,7 @@
 
 // Optimal Approach -
 
-import java.util.*;;
+import java.util.*;
 
 public class LeadersInAnArray {
     public static void main(String[] args) {

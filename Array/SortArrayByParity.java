b2a5c0425d273e2ff2d8c085810c13ac72f6fa0d
suraj rawat <surajrwat001@gmail.com>
2026-08-22 14:42:59 +0530

    fixed code

--- Array/SortArrayByParity.java
+++ Array/SortArrayByParity.java
@@ -25,7 +25,7 @@ public class SortArrayByParity {
         }
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 == 1) {
+            if (arr[i] % 2 != 0) {
                 res[j] = arr[i];
                 j++;
             }
@@ -36,3 +36,7 @@ public class SortArrayByParity {
     }
 
 }
+
+
+// Time complexity - 0(n)
+// Space complexity - 0(n)
--- Array/SquaresOfSortedArray.java
+++ Array/SquaresOfSortedArray.java
@@ -1,29 +1,39 @@
 // Squares of a Sorted Array
 
-import java.util.Arrays;
-
-public class SquaresOfSortedArray {
+class SquaresOfSortedArray {
     public static void main(String[] args) {
-
         int[] nums = { -4, -1, 0, 3, 10 };
+        int[] resultArr = square(nums);
 
-        int n = nums.length;
-        int[] res = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            res[i] = nums[i] * nums[i];
-        }
-
-        Arrays.sort(res);
-
-        System.out.print("Rotated array: [");
-        for (int i = 0; i < res.length; i++) {
-            System.out.print(res[i]);
-            if (i < res.length - 1) {
+        System.out.print("Sorted squares: [");
+        for (int i = 0; i < resultArr.length; i++) {
+            System.out.print(resultArr[i]);
+            if (i < resultArr.length - 1) {
                 System.out.print(", ");
             }
         }
         System.out.println("]");
+    }
 
+    public static int[] square(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0, right = n - 1;
+
+        for (int k = n - 1; k >= 0; k--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[k] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[k] = nums[right] * nums[right];
+                right--;
+            }
+        }
+
+        return result;
     }
 }
+
+// Time complexity - 0(n)
+// Space complexity - 0(n)
